44054ca8afe60388bf194138478a7f3bd7ddc0d6
sneif93 <sneif93@gmail.com>
2022-07-03 15:15:06 -0500

    add_models_and_controllers

---
+++ app/Http/Controllers/CountrieController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Countrie;
+use Illuminate\Http\Request;
+
+class CountrieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Countrie  $countrie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Countrie $countrie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Countrie  $countrie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Countrie $countrie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Countrie  $countrie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Countrie $countrie)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/EmployeeJobPositionController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EmployeeJobPosition;
+use Illuminate\Http\Request;
+
+class EmployeeJobPositionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\EmployeeJobPosition  $employeeJobPosition
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EmployeeJobPosition $employeeJobPosition)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\EmployeeJobPosition  $employeeJobPosition
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EmployeeJobPosition $employeeJobPosition)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\EmployeeJobPosition  $employeeJobPosition
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EmployeeJobPosition $employeeJobPosition)
+    {
+        //
+    }
+}
